385c2969bb89340a5895ae827baa0787caf1f2fc
guruJosh <horwath_cosmin@yahoo.com>
2019-09-26 16:56:13 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,65 @@
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var session = require("express-session");
+
+const login = require('./routes/login');
+
+
+var app = express();
+
+// Serve static files from the React app
+app.use(express.static(path.join(__dirname, 'client/build')));
+//app.use(express.static(path.join(__dirname, 'public')));
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json({limit: '50mb'}));
+app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
+app.use(cookieParser());
+app.use(session({
+  secret: "TKRv0IJs=HYqrvagQ#&!F!%V]Ww/4KiVs$s,<<MX",
+  resave: true,
+  saveUninitialized: true
+ }));
+
+
+app.use('/login', login);
+
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+// The "catchall" handler: for any request that doesn't
+// match one above, send back React's index.html file.
+/*app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname+'/client/build/index.html'));
+});*/
+
+const port = process.env.PORT || 5000;
+app.listen(port);
+
+module.exports = app;
---
+++ client/src/App.js
@@ -0,0 +1,37 @@
+import React from 'react';
+
+
+
+export default class App extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+          login: false
+      }
+  
+    }
+
+
+
+    async componentWillMount(){
+       
+    }
+
+    async componentDidMount(){
+     
+    }
+
+   
+
+    render() {
+       return (
+        <React.Fragment>
+          Hello Hermes
+        </React.Fragment>
+        );
+      }
+
+
+
+    
+}
